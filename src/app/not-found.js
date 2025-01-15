af09ee32eeb5399902cc16f0edafbef654500b21
YuvanVH <vanessa-huynh@hotmail.com>
2025-01-15 10:01:22 +0100

    404 becomes not-found instead

---
+++ src/app/not-found.js
@@ -0,0 +1,69 @@
+// src/app/404/page.js
+import { fetchErrorPage } from './lib/graphql';
+import Header from './components/Header';
+
+export default async function ErrorPage() {
+  const errorPage = await fetchErrorPage();
+
+  if (!errorPage) {
+    return (
+      <div>
+        <h1>404 - Page not found</h1>
+        <p>We couldn't find the page you were looking for.</p>
+      </div>
+    );
+  }
+
+  const slogan = errorPage?.slogan
+    ? errorPage.slogan.json.content.map((block) =>
+      block.content.map((innerBlock) => innerBlock.value).join('')
+    ).join('\n')
+    : 'Page not found.';
+
+  const renderErrorMessage = (content) => {
+    return content.map((block, index) => {
+      switch (block.nodeType) {
+        case 'heading-2':
+          return (
+            <h2 key={index}>
+              {block.content.map((innerBlock, i) => innerBlock.value).join('')}
+            </h2>
+          );
+        case 'paragraph':
+          return (
+            <p key={index}>
+              {block.content.map((innerBlock, i) => {
+                if (innerBlock.nodeType === 'text') {
+                  return innerBlock.value;
+                } else if (innerBlock.nodeType === 'hyperlink') {
+                  return (
+                    <a href={innerBlock.data.uri} key={i}>
+                      {innerBlock.content.map((linkText) => linkText.value).join('')}
+                    </a>
+                  );
+                }
+                return ''; // Default case att hantera okända nodeType
+              })}
+            </p>
+          );
+        default:
+          return null; // ignorerar unsupported node types
+      }
+    });
+  };
+
+  return (
+    <div>
+      <Header
+        title={errorPage.title}
+        slogan={slogan}
+        backgroundImage={errorPage?.backgroundImage?.url || '/default-image.jpg'}
+        logo={errorPage?.logo?.url || '/default-logo.png'}
+      />
+      <div>
+        {/* Renderar error message content */}
+        {renderErrorMessage(errorPage?.errorMessage?.json?.content) || 'Error message not available'}
+      </div>
+    </div>
+  );
+}
